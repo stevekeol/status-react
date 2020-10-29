040c3daa48a4317ccd45ff813df1cdc74133d288
Gheorghe Pinzaru <feross95@gmail.com>
2020-10-29 10:42:28 +0300

    Use different library

--- src/status_im/ui/screens/chat/image/preview/views.cljs
+++ src/status_im/ui/screens/chat/image/preview/views.cljs
@@ -1,91 +1,61 @@
 (ns status-im.ui.screens.chat.image.preview.views
   (:require [status-im.ui.components.colors :as colors]
             [status-im.ui.components.react :as react]
-            [re-frame.core :as re-frame]
             [reagent.core :as reagent]
             [quo.core :as quo]
-            [quo.react-native :as rn]
-            [quo.animated :as animated]
             [status-im.i18n :as i18n]
             [status-im.ui.components.icons.vector-icons :as icons]
             [status-im.ui.screens.chat.sheets :as sheets]
             [quo.components.safe-area :as safe-area]
-            ["react-native" :as rrn]
-            ["react-native-image-pan-zoom" :default pan-zoom]))
+            ["react-native-image-viewing" :default image-viewing]))
 
-(defn preview-image []
+(defn footer []
   (let [show-sheet (reagent/atom false)]
+    (fn [{:keys [on-close message]}]
+     [safe-area/consumer
+      (fn [insets]
+        [react/view {:style {:position       "absolute"
+                             :left           0
+                             :right          0
+                             :bottom         0
+                             :padding-bottom (:bottom insets)}}
+         [react/view {:style {:flex-direction     :row
+                              :padding-horizontal 24
+                              :justify-content    :center
+                              :align-items        :center}}
+          [react/touchable-highlight {:on-press on-close
+                                      :style    {:background-color   colors/black-transparent-86
+                                                 :padding-horizontal 24
+                                                 :padding-vertical   11
+                                                 :border-radius      44}}
+           [quo/text {:style {:color colors/white-persist}}
+            (i18n/label :t/close)]]
 
-    (fn [{{:keys [content] :as message} :message
-          visible                       :visible
-          on-close                      :on-close
-          dimensions                    :dimensions}]
-      (let [{screen-width  :width
-             screen-height :height} @(re-frame/subscribe [:dimensions/window])
-            k                       (if (< screen-height (second dimensions))
-                                      (/ (first dimensions) screen-width)
-                                      (/ (second dimensions) screen-height))
-            width                   (/ (first dimensions) k)
-            height                  (/ (second dimensions) k)
-            animated-y              (animated/value 0)
-            on-move                 (fn [^js move]
-                                      (when (= (.-type move) "onPanResponderMove")
-                                        (animated/set-value animated-y (.-positionY move))))
-            reset                   (fn []
-                                      (animated/set-value animated-y 0))
-            opacity                 (animated/interpolate animated-y {:inputRange  [0 (/ screen-height 2)]
-                                                                      :outputRange [1 0]
-                                                                      :extrapolate (:clamp animated/extrapolate)})]
-        [rn/modal {:visible         visible
-                   :on-dismiss      reset
-                   :on-equest-close on-close
-                   :transparent     true}
-         [safe-area/consumer
-          (fn [insets]
-            [react/view {:style {:flex 1}}
-             [animated/view {:style {:position         "absolute"
-                                     :top              0
-                                     :bottom           0
-                                     :left             0
-                                     :right            0
-                                     :background-color colors/black-persist
-                                     :opacity          opacity}}]
-             [:> pan-zoom {:crop-width        screen-width
-                           :crop-height       screen-height
-                           :image-width       width
-                           :image-height      height
-                           :enable-swipe-down true
-                           :on-move           on-move
-                           :responder-release #(js/setTimeout reset 100)
-                           :on-swipe-down     (fn []
-                                                (on-close))
-                           :useNativeDriver   true}
-              [react/image {:source {:uri (:image content)}
-                            :style  {:width  width
-                                     :height height}}]]
-             [react/view {:style {:position       "absolute"
-                                  :left           0
-                                  :right          0
-                                  :bottom         0
-                                  :padding-bottom (:bottom insets)}}
-              [react/view {:flex-direction     :row
-                           :padding-horizontal 8
-                           :justify-content    :space-between
-                           :align-items        :center}
-               [react/view {:width 64}]
-               [quo/button {:on-press   on-close
-                            :type       :secondary
-                            :text-style {:color colors/white-persist}}
-                (i18n/label :t/close)]
+          ;; NOTE(Ferossgp): If we use global bottom sheet, then it is rendered under the preview
+          [quo/bottom-sheet {:visible?  @show-sheet
+                             :on-cancel #(reset! show-sheet false)}
+           [sheets/image-long-press message #(do (reset! show-sheet false)
+                                                 (on-close))]]
+          [react/touchable-highlight
+           {:on-press #(reset! show-sheet true)
+            :style    {:background-color colors/black-transparent-86
+                       :border-radius    44
+                       :padding          8
+                       :position         :absolute
+                       :bottom           0
+                       :right            24}}
+           [icons/icon :main-icons/more {:container-style {:width  24
+                                                           :height 24}
+                                         :color           colors/white-persist}]]]])])))
 
-               ;; NOTE(Ferossgp): If we use global bottom sheet, then it is rendered under the preview
-               [quo/bottom-sheet {:visible?  @show-sheet
-                                  :on-cancel #(reset! show-sheet false)}
-                [sheets/image-long-press message #(do (reset! show-sheet false)
-                                                      (on-close))]]
-               [react/touchable-highlight
-                {:on-press #(reset! show-sheet true)}
-                [icons/icon :main-icons/more {:container-style {:width  24
-                                                                :height 24
-                                                                :margin 20}
-                                              :color           colors/white-persist}]]]]])]]))))
+(defn preview-image [{{:keys [content] :as message} :message
+                      visible                       :visible
+                      on-close                      :on-close}]
+  [:> image-viewing {:images           #js [#js {:uri (:image content)}]
+                     :on-request-close on-close
+                     :hideHeaderOnZoom false
+                     :hideFooterOnZoom false
+                     :HeaderComponent  #(reagent/as-element [:<>]) ; NOTE: Keep it to remove default header
+                     :FooterComponent  #(reagent/as-element [footer {:on-close on-close
+                                                                     :message  message}])
+                     :visible          visible}])
